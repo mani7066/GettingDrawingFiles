a6578d27bb9ba528fcf4ef58f95ba755a0663934
mani <138546173+mani7066@users.noreply.github.com>
2024-06-28 11:47:55 +0530

    Add files via upload

---
+++ GettingDrawingFiles/GettingDrawingFiles/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace GettingDrawingFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(376, 40);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(467, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(56, 149);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(582, 236);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 476);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ GettingDrawingFiles/GettingDrawingFiles/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Inventor;
+
+namespace GettingDrawingFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.ShowNewFolderButton = true;
+            DialogResult dialogres = folderDlg.ShowDialog();
+            DirectoryInfo dir = new DirectoryInfo(folderDlg.SelectedPath);
+            FileInfo[] files = dir.GetFiles("*.idw");
+            if (dialogres == DialogResult.OK)
+            {
+                textBox1.Text = folderDlg.SelectedPath;
+                foreach(var file in files)
+                {
+                    textBox2.Text  = textBox2.Text + file.ToString() + "\r\n";
+                }
+            }
+            Inventor.Application invApp = (Inventor.Application)System.Runtime.InteropServices.Marshal.GetActiveObject("Inventor.Application");
+            try
+            {
+                foreach (var file in files)
+                {
+                    string fullfilepath = folderDlg.SelectedPath + "\\" + file;
+                    Logger(fullfilepath);
+                    Inventor.Document Doc = invApp.Documents.Open(fullfilepath, true);
+                    DrawingDocument drwdoc = (DrawingDocument)Doc;
+                    PropertySet drawprop = drwdoc.PropertySets["Inventor User Defined Properties"];
+
+                    string PropVal = "";
+                    string PropName = "";
+                    Property propname = null;
+                    string[] fieldnames = {"Revision Number", "1-1-Status", "2-1-Status", "3-1-Status", "4-1-Status", "5-1-Status", "6-1-Status", "7-1-Status", "8-1-Status", "Änderungstext1", "Änderungstext2",
+             "Änderungstext3", "Änderungstext4", "Änderungstext5", "Änderungstext6", "Änderungstext7", "Änderungstext8", "1-3-Date", "2-3-Date", "3-3-Date", "4-3-Date", "5-3-Date", "6-3-Date", "7-3-Date", "8-3-Date",
+             "1-4-Name", "2-4-Name", "3-4-Name", "4-4-Name", "5-4-Name", "6-4-Name", "7-4-Name", "8-4-Name"};
+                    foreach (var names in fieldnames)
+                    {
+                        try
+                        {
+                            propname = drawprop[names];
+                            Sheet osheet = drwdoc.Sheets[1];
+                            DrawingView drawview = osheet.DrawingViews[1];
+                            Doc = (Document)drawview.ReferencedDocumentDescriptor.ReferencedDocument;
+                            PropertyAdd(Doc, propname, names, propname.Value);
+                        }
+                        catch
+                        {
+                            Sheet osheet = drwdoc.Sheets[1];
+                            DrawingView drawview = osheet.DrawingViews[1];
+                            Doc = (Document)drawview.ReferencedDocumentDescriptor.ReferencedDocument;
+                            PropertyAdd(Doc, propname, names, "");
+                        }
+                    }
+                    Doc.Save();
+                    Doc.Close();
+                    drwdoc.Close();
+                }
+                invApp.Quit();
+            }
+            catch(Exception ex)
+            {
+                Logger(ex.ToString());
+            }
+        }
+        public static void PropertyAdd(Document Doc, Property prop,string propname, object propvalue)
+        {
+            bool propExists = true;
+            PropertySet customPropSet = Doc.PropertySets["Inventor User Defined Properties"];
+            try
+            {
+                prop = customPropSet[propname];
+            }
+            catch (Exception ex)
+            {
+                propExists = false;
+            }
+            if (!propExists)
+            {
+                prop = customPropSet.Add(propvalue, propname, null);
+            }
+            else
+            {
+                prop.Value = propvalue;
+            }
+        }
+        public static void FindDyr(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\mani\logs\");
+            if (!dir.Exists)
+            {
+                dir.Create();
+            }
+        }
+        public static void Logger(string lines)
+        {
+            string path = "C:/mani/logs/";
+            FindDyr(path);
+            string fileName = DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Year.ToString() + "_Logs.txt";
+            try
+            {
+                System.IO.StreamWriter file = new System.IO.StreamWriter(path + fileName, true);
+                file.WriteLine(DateTime.Now.ToString() + ": " + lines);
+                file.Close();
+            }
+            catch (Exception) { }
+        }
+    }
+}
